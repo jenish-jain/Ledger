26a6efc89d279a91c5568d433390841f39b5ae15
jenish-jain <jenishjain@rocketmail.com>
2020-04-19 13:07:56 +0530

    fixed config

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     //   host_url:"http://3.6.126.6",
-    host_url = "https://ledger-dev.herokuapp.com/",
+    host_url : "https://ledger-dev.herokuapp.com/",
     mongo: {
         write_uri: "mongodb+srv://jenish:Juju6397@cluster0-e1fiw.mongodb.net/test?retryWrites=true&w=majority"
     },
